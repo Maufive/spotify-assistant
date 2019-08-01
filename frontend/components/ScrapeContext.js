94876f4bca8408ae9f8e49bfee7e2b9b7be62d57
Niklas Albinsson <albinssonniklas@gmail.com>
2019-08-01 15:28:18 +0200

    added library for interacting with spotifys api

--- frontend/components/ScrapeContext.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-export const ScrapeContext = React.createContext();
-
-export const ScrapeProvider = ScrapeContext.Provider;
-// const ScrapeConsumer = ScraperContext.Conusmer;
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,11 +1,13 @@
 import Page from "../components/Page";
 import Lyrics from "../components/Lyrics";
+import Login from "../components/auth/Login";
 
 export default function Home() {
 	return (
 		<Page>
 			<h1>Homepage</h1>
-			<Lyrics />
+			<Login />
+			{/* <Lyrics /> */}
 		</Page>
 	);
 }
