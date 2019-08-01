eb96f41dba44f1dfa60a539d3f184ec616ea9f64
Niklas Albinsson <albinssonniklas@gmail.com>
2019-08-01 18:42:46 +0200

    added state for currentsong

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,14 +1,18 @@
 import { TokenProvider } from "./Context";
 import useLogin from "./hooks/useLogin";
+import useCurrentSong from "./hooks/useCurrentSong";
 
 export default function Page({ children }) {
 	// const lyrics = useLyrics();
 	const { token } = useLogin();
+	const { song } = useCurrentSong(token);
 	return (
 		<TokenProvider
 			value={{
 				useLogin,
-				token
+				token,
+				currentSong: song,
+				useCurrentSong
 			}}
 		>
 			<div className="page">{children}</div>
