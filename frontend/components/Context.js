8a5fafd93c64b8ed9254827d753487c2fb2467ea
Niklas Albinsson <albinssonniklas@gmail.com>
2019-08-01 15:26:54 +0200

    Added context for login token

---
+++ frontend/components/Context.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const LyricsContext = React.createContext();
+
+export const LyricsProvider = LyricsContext.Provider;
+
+export const TokenContext = React.createContext();
+
+export const TokenProvider = TokenContext.Provider;
+
+export const TokenConsumer = TokenContext.Consumer;
+// const ScrapeConsumer = ScraperContext.Conusmer;
