b5c0bc541233f3e90f59ef21c670ae3e19372212
Niklas Albinsson <albinssonniklas@gmail.com>
2019-08-03 13:56:13 +0200

    Added new hook for genius data fetching

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,11 +1,14 @@
 import { TokenProvider } from "./Context";
 import useLogin from "./hooks/useLogin";
 import useCurrentSong from "./hooks/useCurrentSong";
+import useGenius from "./hooks/useGenius";
 
 export default function Page({ children }) {
 	// const lyrics = useLyrics();
 	const { token } = useLogin();
 	const { song } = useCurrentSong(token);
+	const { genius } = useGenius(song);
+
 	return (
 		<TokenProvider
 			value={{
