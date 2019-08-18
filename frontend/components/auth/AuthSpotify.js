fd16b3ffddfe2cbd456ab8083c9e3df31ebae804
Niklas Albinsson <albinssonniklas@gmail.com>
2019-08-18 13:22:56 +0200

    Extraced loginbutton to own component

--- frontend/components/auth/AuthSpotify.js
+++ frontend/components/auth/AuthSpotify.js
@@ -5,30 +5,9 @@ import { TokenContext } from "../Context";
 import styled from "styled-components";
 import { motion } from "framer-motion";
 import useLogin from "../hooks/useLogin";
+import { LoginButton } from "../Buttons";
 
 export const authEndpoint = "https://accounts.spotify.com/authorize?";
-
-const LoginButton = styled(Link)`
-	font-family: "Open Sans";
-	font-weight: 400;
-	text-transform: uppercase;
-	background: none;
-	border: none;
-	color: ${props => props.theme.anotherGrey};
-	font-size: 2rem;
-	transition: all 300ms ease-out;
-	&:hover {
-		cursor: pointer;
-	}
-`;
-
-const Underline = styled.span`
-	width: 100%;
-	height: 2px;
-	background: ${props => props.theme.anotherGrey};
-	margin-top: 5px;
-	transition: all 300ms ease-out;
-`;
 
 const Container = styled(motion.div)`
 	display: flex;
@@ -53,23 +32,15 @@ export default function LoginSpotify() {
 	};
 
 	return (
-		<div>
-			{/* <a
-				href={`${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
-					"%20"
-				)}&response_type=token&show_dialog=true`}
-			> */}
-			<Container
-				whileHover={{ scale: 1.05 }}
-				whileTap={{ scale: 0.95 }}
-				transition={{ type: "spring", stiffness: 200 }}
-			>
-				<LoginButton href="http://localhost:2093/auth/login">
+		<Container
+			whileHover={{ y: "-3px" }}
+			transition={{ type: "spring", stiffness: 300, damping: 200 }}
+		>
+			<LoginButton>
+				<Link href="http://localhost:2093/auth/login">
 					<a>LOGIN WITH SPOTIFY</a>
-				</LoginButton>
-				<Underline />
-			</Container>
-			{/* </a> */}
-		</div>
+				</Link>
+			</LoginButton>
+		</Container>
 	);
 }
